6d8f52f49c092d103784650d3a338275920d7093
Bharath Battini <90249031+bharath712@users.noreply.github.com>
2023-03-10 23:57:31 +0530

    FirstCommit

---
+++ explore.ts
@@ -0,0 +1,9 @@
+var i=20;
+console.log(i);
+console.log(typeof(i));
+i=true;
+console.log(i);
+console.log(typeof(i));
+i="Bharath";
+console.log(i);
+console.log(typeof(i));
---
+++ stringliterals.js
@@ -0,0 +1,6 @@
+var str1 = "Hello World"
+var str = 'Hello World!'
+var str2 = `Hello World!!`
+console.log(str1)
+console.log(str2)
+console.log(str)
